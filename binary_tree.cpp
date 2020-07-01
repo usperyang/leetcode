0f7b56553947180fb093df7076fad3f20fd74479
yangyuchao <yangyuchao@momenta.ai>
2020-07-01 15:59:08 +0800

    add

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -2,10 +2,23 @@
 #include <cstdio>
 using namespace std;
 
-class ListNode{
+struct  ListNode
+{
+    /* data */
     int val;
     ListNode *next;
+    ListNode(int x) : val(x), next(NULL){}
 };
+
+struct TreeNode
+{
+    /* data */
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL){}
+};
+
 
 void traverse(ListNode *head){
     for (ListNode *p = head; p != NULL; p = p.next){
