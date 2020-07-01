bc38ce1a2fcee537b71e53d5e1147393167a2deb
yangyuchao <yangyuchao@momenta.ai>
2020-07-01 13:54:47 +0800

    first commit

---
+++ binary_tree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+class ListNode{
+    int val;
+    ListNode *next;
+};
+
+void traverse(ListNode *head){
+    for (ListNode *p = head; p != NULL; p = p.next){
+
+    }
+}
+
+int main(){
+
+    return 0;
+}
